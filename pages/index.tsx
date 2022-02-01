777d45288b5bc7fa8223ad489c054a2ccc6be2f8
Ryan Irilli <ryan.irilli@canoo.com>
2022-01-31 21:10:30 -0800

    update meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,21 +6,21 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Canoo Lifestyle Vehicle rendered using react-three-fiber</title>
+        <title>Mercedes GLS 580 rendered using react-three-fiber</title>
         <meta
           prefix="og: http://ogp.me/ns#"
           property="og:title"
-          content="Canoo Lifestyle Vehicle rendered using react-three-fiber"
+          content="Mercedes GLS 580 rendered using react-three-fiber"
         />
         <meta
           prefix="og: http://ogp.me/ns#"
           property="og:image"
-          content="https://canoo-explorer-omega.vercel.app/meta-image.jpg"
+          content="https://vehicle-explorer.vercel.app/meta-image.jpg"
         />
         <meta
           prefix="og: http://ogp.me/ns#"
           property="og:url"
-          content="https://canoo-explorer-omega.vercel.app/"
+          content="https://vehicle-explorer.vercel.app/"
         />
       </Head>
       <Scene />
